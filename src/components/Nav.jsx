e12be8cf48203fe8aa4d62c35901d99cbe1bada4
Tis Lais <tistististis@gmail.com>
2021-11-14 18:00:01 -0800

    make navitem component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import NavItem from './NavItem';
 
 const links = ['Home', 'Products', 'Docs', 'Company', 'Contact']
 const navItems = links.map(item => 
-  <li>{item}</li>
+  <NavItem item={item} />
 )
 
 const Nav = () => {
   return (
     <nav>
-      <ul>
+      <ul className={ul}>
         {navItems}
       </ul>
     </nav>
   )
 }
+
+const ul = `
+  bg-gray-800
+`
+
+const li = ``
 
 export default Nav;
---
+++ src/components/NavItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavItem = ({ item }) => {
+  return (
+    <li>{item}</li>
+  )
+}
+
+export default NavItem
