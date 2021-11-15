963874a9d0b12f744483bfc8a6c35bad151b2e7a
Tis Lais <tistististis@gmail.com>
2021-11-14 17:40:32 -0800

    base header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from 'react'
+import { MenuIcon } from '@heroicons/react/solid'
 
 const Header = () => {
   return (
-    <header>
-      <h1>Project Owlfeather</h1>
+    <header className={header}>
+      <MenuIcon className={menu} />
+      <h1 className={h1}>Project Owlfeather</h1>
     </header>
   )
 }
+
+const header = `
+  p-4 
+
+  bg-gradient-to-r 
+  from-green-500 
+  to-blue-500 
+
+  text-white
+
+  border-b-8 
+  border-black 
+  border-opacity-30
+
+  flex
+  gap-4
+`
+
+const h1 = `
+  font-semibold
+  uppercase
+  tracking-wider
+`
+
+const menu = `
+  h-6
+  hover:text-green-700
+`
 
 export default Header;
